b6be606edbdaac7db9405bce901b3b52c70ead07
Casey Leask <caseyleask@gmail.com>
2012-07-03 17:54:45 +1000

    Changes the site mapping to home

--- config/routes.rb
+++ config/routes.rb
@@ -48,7 +48,7 @@ WeddingInvite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'site#home'
+  root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
