dd63fc855f6338c658bf2cb1fc59d585f0fae1f7
Casey Leask <caseyleask@gmail.com>
2012-07-09 20:53:30 +1000

    Used named routes in the header after naming them

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 WeddingInvite::Application.routes.draw do
-  get "gifts/info"
-
+  match 'gifts' => 'gifts#info', :as => :gifts
   match 'rsvp' => 'rsvp#details', :as => :rsvp
   match 'ceremony_and_reception' => 'ceremony_and_reception#info', :as => :ceremony_and_reception
 
@@ -53,7 +52,7 @@ WeddingInvite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => 'home#index'
+  root :to => 'home#index', :as => :home
 
   # See how all your routes lay out with "rake routes"
 
